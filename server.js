09f416f230150761881521d8e35338b48713846e
roelkers <rufus.oelkers@gmail.com>
2017-06-15 20:44:03 +0200

    first commit

--- server.js
+++ server.js
@@ -2,13 +2,24 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 
+var upload = multer();
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res,next){
+  console.log(req.body);
+  res.json({
+    name : req.file.originalname,
+    size : req.file.size,
+    type : req.file.mimetype
+  });
+})
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
